a527a2cadb9bdc8bcdf8107bbfd282858252fc24
John Vincent S. Guioguio <125118778+jevguio@users.noreply.github.com>
2025-05-18 16:31:28 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 Route::get('/hello-plugin', function () {
+
+    if (Auth::check()) {
+        // User is logged in
+        $user = Auth::user();
+    }
     
+
     return view('helloworld::helloworld'); // ← explanation below
-})->middleware(['auth'])->name('test.plugin');
+})->name('test.plugin');
 
