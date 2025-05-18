0d8cb2e5e930a748b39b4e15b7c57d647aa881fa
John Vincent S. Guioguio <125118778+jevguio@users.noreply.github.com>
2025-05-18 16:22:40 +0800

    middleware added

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/hello-plugin', function () {
     
     return view('helloworld::helloworld'); // ← explanation below
-})->name('test.plugin');
+})->middleware(['auth', 'verified'])->name('test.plugin');
 
