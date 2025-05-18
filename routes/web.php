82f46d55429199840f919bfff01958e8ebb1cce2
John Vincent S. Guioguio <125118778+jevguio@users.noreply.github.com>
2025-05-18 16:38:46 +0800

    middleware fixed

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,13 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-Route::get('/hello-plugin', function () {
+Route::middleware(['web'])->group(function () {
+    Route::get('/hello-plugin', function () {
+        if (Auth::check()) {
+            $user = Auth::user();
+            // Do something with $user if needed
+        }
 
-    if (Auth::check()) {
-        // User is logged in
-        $user = Auth::user();
-    }
-    
-
-    return view('helloworld::helloworld'); // ← explanation below
-})->name('test.plugin');
-
+        return view('helloworld::helloworld');
+    })->name('test.plugin');
+});
