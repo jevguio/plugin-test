827c08420d8b88bbb190afb7c9a9f77d9311f25c
John Vincent S. Guioguio <125118778+jevguio@users.noreply.github.com>
2025-05-18 15:23:45 +0800

    new v

--- PluginServiceProvider.php
+++ PluginServiceProvider.php
@@ -7,14 +7,29 @@ use App\PluginHook;
 
 class PluginServiceProvider extends ServiceProvider
 {
+    
     public function boot()
     {
+        
+        $jsonPath = __DIR__ . '/../plugin.json';
         // Register plugin route
         include __DIR__ . '/routes/web.php';
 
         // Register a dashboard button
 
-        PluginHook::registerPlugin('Test Plugin', 'HelloWorld', '1.0.2', ' Testing Plugin Developent.', 'https://github.com/jevguio/plugin-test.git');
+        PluginHook::registerPlugin(
+            $data['name'] ?? 'Unknown',
+            basename(dirname(__DIR__)), // or $data['slug'] if you have it
+            $data['version'] ?? '0.0.0',
+            $data['description'] ?? '',
+            $data['gitUrl'] ?? ''
+        );
         PluginHook::addTopMenu('Test Plugin', 'test.plugin');
+        
+        $this->loadViewsFrom(__DIR__.'/../resources/view', 'helloworld');
+
+        // Register routes
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
     }
+    
 }
