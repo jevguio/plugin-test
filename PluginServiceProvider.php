d0162497e8792f531af97c5e656ecbb7371f84e8
John Vincent S. Guioguio <125118778+jevguio@users.noreply.github.com>
2025-05-18 15:29:45 +0800

    fixed

--- PluginServiceProvider.php
+++ PluginServiceProvider.php
@@ -11,25 +11,25 @@ class PluginServiceProvider extends ServiceProvider
     public function boot()
     {
         
-        $jsonPath = __DIR__ . '/../plugin.json';
-        // Register plugin route
-        include __DIR__ . '/routes/web.php';
+        $jsonPath = __DIR__ . '/plugin.json';
 
-        // Register a dashboard button
+        if (file_exists($jsonPath)) {
+            $data = json_decode(file_get_contents($jsonPath), true);
 
-        PluginHook::registerPlugin(
-            $data['name'] ?? 'Unknown',
-            basename(dirname(__DIR__)), // or $data['slug'] if you have it
-            $data['version'] ?? '0.0.0',
-            $data['description'] ?? '',
-            $data['gitUrl'] ?? ''
-        );
+            PluginHook::registerPlugin(
+                $data['name'] ?? 'Unknown',
+                basename(dirname(__DIR__)), // or $data['slug'] if you have it
+                $data['version'] ?? '0.0.0',
+                $data['description'] ?? '',
+                $data['gitUrl'] ?? ''
+            );
+        }
         PluginHook::addTopMenu('Test Plugin', 'test.plugin');
         
-        $this->loadViewsFrom(__DIR__.'/../resources/view', 'helloworld');
+        $this->loadViewsFrom(__DIR__.'/resources/view', 'helloworld');
 
         // Register routes
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
     }
     
 }
