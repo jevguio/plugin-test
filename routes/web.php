38244c2def64d748ef813d6f3a929bf061d92856
John Vincent S. Guioguio <125118778+jevguio@users.noreply.github.com>
2025-05-18 16:25:35 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/hello-plugin', function () {
     
     return view('helloworld::helloworld'); // ← explanation below
-})->middleware(['auth', 'verified'])->name('test.plugin');
+})->middleware(['auth'])->name('test.plugin');
 
